f9eb5aad60cb38caa8853101cb4c94e162c570e7
Your Name <youremail@example.com>
2026-06-18 20:45:22 -0400

    Speed and Analytics Added from vercel

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,5 +1,7 @@
 import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
+import { Analytics } from "@vercel/analytics/next";
+import { SpeedInsights } from "@vercel/speed-insights/next";
 import "./globals.css";
 import BannedAccountShell from "./components/BannedAccountShell"
 import SentryIdentifyUser from "./components/SentryIdentifyUser"
@@ -75,6 +77,8 @@ export default function RootLayout({
             </GettingStartedProgressProvider>
           </UserProfileProvider>
         </ToastRoot>
+        <Analytics />
+        <SpeedInsights />
       </body>
     </html>
   );
